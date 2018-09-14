fc776eab08e3f6ffa78d27d4de27a3cf65a08dfd
Josh Sanderson <josh@jgstechnical.com>
2018-09-13 20:26:24 -0400

    Commit dist for publishing

---
+++ env/os-windows/env-common.bat
@@ -0,0 +1,13 @@
+@echo off
+
+SET PROJECT_HOME=C:\Projects\misc\website-jgstechnical
+
+
+SET PATH=C:\Develop\nodejs;%PATH%
+SET PATH=%APPDATA%\npm;%PATH%
+SET PATH=%PROJECT_HOME%\node_modules\.bin;%PATH%
+
+SET PATH=%PATH%;C:\Cygwin64\bin
+SET PATH=%PATH%;C:\Program Files\Git\bin
+
+cd %PROJECT_HOME%
