d1ebcbc821e3fc3b3ff94b40c0b19d0d10b5a763
kaliatech <josh@jgstechnical.com>
2024-04-24 16:56:11 -0400

    Add custom colors (wip)

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,8 +6,27 @@ import daisyUiPlugin from 'daisyui'
 
 export default {
   darkMode: 'selector', // or 'media'
+
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        jgsblue: {
+          50: '#FAFCFF',
+          100: '#F0F6FF',
+          200: '#E0EDFF',
+          300: '#D1E3FF',
+          400: '#C7DDFF',
+          500: '#B6D3FF',
+          600: '#61A0FF',
+          700: '#0569FF',
+          800: '#0045AD',
+          900: '#002357',
+          950: '#00122E',
+        },
+      },
+    },
+  },
   extend: {},
   plugins: [
     daisyUiPlugin,
