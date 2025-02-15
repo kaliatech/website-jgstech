b0117ee6a875dd8c910f922c4e791f4d53846004
Josh Sanderson <josh@jgstechnical.com>
2025-02-15 11:44:39 -0500

    Configure passthroughImageService

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig, passthroughImageService } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 
 import react from '@astrojs/react'
@@ -6,6 +6,10 @@ import react from '@astrojs/react'
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
+  image: {
+    service: passthroughImageService(),
+  },
+
   // vite: {
   //   server: {
   //     fs: {
