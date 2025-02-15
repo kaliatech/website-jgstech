cc7817948435d28dad16503047cdbcba12810a28
Josh Sanderson <josh@jgstechnical.com>
2025-02-15 11:01:23 -0500

    Tweak styling on contact page

--- src/components-client/ContactMap/ContactMap.tsx
+++ src/components-client/ContactMap/ContactMap.tsx
@@ -3,10 +3,10 @@ export default function MapChart() {
     <iframe
       title={'Contact Map'}
       src="https://www.google.com/maps/d/u/0/embed?mid=1TEsi5IaoFPmjnznqwtNBm1w95vwimlqv"
-      width="640"
+      width="100%"
       height="480"
       allowFullScreen
-      className="aspect-square max-h-[640px] w-full"
+      className="aspect-square max-h-[640px] max-w-[75rem]"
     />
   )
 }
